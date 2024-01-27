415ea275912e1d4a581be032523b07c7289fdf9a
Fillipe Diord <fillipe.diord@hotmail.com>
2024-01-27 16:07:01 -0300

    feat: 🎸 create image component and styles
    create image component and styles

---
+++ src/components/Image/index.tsx
@@ -0,0 +1,11 @@
+import { ImgHTMLAttributes } from "react";
+
+import './styles.css';
+
+export function Image({ ...props }: ImgHTMLAttributes<HTMLImageElement>) {
+  return (
+    <>
+      <img {...props} />
+    </>
+  )
+}
